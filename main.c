5ccb7c87eefdd442f4c196a931e2848c08d9452e
Thiago Assis <thiago.assisfernandes@gmail.com>
2026-08-30 10:04:23 -0300

    done

--- main.c
+++ main.c
@@ -2,24 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+static void replaceComma(const char *input, char *output, size_t out_size) {
+  char *ptrOut = output;
+  const char *ptrIn = input;
+  while ((size_t)(ptrOut - output) < out_size - 4 && *ptrIn) { // safety margin
+    if (*ptrIn == ',') {
+      *ptrOut++ = ' ';
+      *ptrOut++ = '|';
+      *ptrOut++ = ' ';
+    } else {
+      *ptrOut++ = *ptrIn;
+    }
+    ptrIn++;
+  }
+  *ptrOut = '\0';
+}
+
 int main(void) {
   char line[1024];
   while (fgets(line, sizeof line, stdin)) {
-    line[strcspn(line, "\n")] = '\0'; // Removing trailing new line characters
+    // Remove trailing newline
+    line[strcspn(line, "\n")] = '\0';
 
-    char out[sizeof(line) * 3]; // Creating an output buffer
-    char *ptrOut = out;         // Pointer to output buffer
-
-    for (const char *ptrS = line; *ptrS; ++ptrS) {
-      if (*ptrS == ',') {
-        *ptrOut++ = ' ';
-        *ptrOut++ = '|';
-        *ptrOut++ = ' ';
-      } else {
-        *ptrOut++ = *ptrS;
-      }
-    }
-    *ptrOut = '\0';
+    char out[sizeof(line) * 3]; // guaranteed enough
+    replaceComma(line, out, sizeof(out));
     printf("%s\n", out);
   }
   return EXIT_SUCCESS;
